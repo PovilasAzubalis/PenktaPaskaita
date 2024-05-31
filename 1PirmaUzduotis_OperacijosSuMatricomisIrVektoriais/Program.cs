ee96d3f726ce6cb14a7894108cce15ed9f4e6ae0
azubalispovilas <azubalispovilas@gmail.com>
2024-05-31 10:22:22 +0300

    Add project files.

---
+++ 1PirmaUzduotis_OperacijosSuMatricomisIrVektoriais/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace PirmaUzduotis_OperacijosSuMatricomisIrVektoriais
+{
+        public class Program
+        {
+                public static void Main(string[] args)
+                {
+
+                }
+
+        }
+
+        public class Matrica(string[] args)
+        {
+                int[,] matrica1 = { { 1, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } };
+
+                int x = matrica1.GetLength(0);
+                int y = matrica1.GetLength(1);
+                int y = matrica1.GetLength(2);
+                IsvestiMatrica(ref int x, ref int y, ref int z);
+
+                static void IsvestiMatrica(ref int x, ref int y, ref int z)
+                {
+                        for (int i = 0; i < x; i++)
+                        {
+                                Console.Write(x);
+
+                                for (int j = 0; j < numbers.GetLength(1); j++)
+                                {
+                                        Console.Write(Matrica.matrica1[i, j] + " ");
+
+                                }
+                                Console.WriteLine();
+                        }
+                }
+
+
+        }
